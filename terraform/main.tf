aad300d6f8a79424699ebb6a2c9c3fe085a34174
faizunRBAI <faizun@royalbengal.ai>
2026-07-19 01:38:26 +0600

    fix destroy attempt 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -308,6 +308,7 @@ resource "aws_security_group_rule" "db_ingress_from_app" {
 resource "aws_ecr_repository" "app" {
   name                 = "${var.project_name}-app"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
